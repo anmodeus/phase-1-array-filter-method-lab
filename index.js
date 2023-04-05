56385b9bd41a7540a4cf18b1c2968296425decb4
Anna Perez <anna.perez.guitar@gmail.com>
2023-04-04 23:41:52 -0700

    uses filter() to return matching results

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching (array, driver){
+    return array.filter(function(matchingDriver){
+    return matchingDriver.toUpperCase()=== driver.toUpperCase()
+    });
+}
+
+function fuzzyMatch(array, char) {
+    return array.filter(function(drivers){
+    return drivers.toUpperCase().indexOf(char.toUpperCase()) === 0;
+    });
+} 
+
+function matchName(array, driver) {
+    return array.filter(function(nameMatch) {
+    return nameMatch.name === driver
+    });
+}
